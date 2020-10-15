f0f72195c11cf71f2911f68cb4c53d439564c0aa
Anna Karutina <anna.karutina@khk.ee>
2020-10-15 09:20:35 +0300

    Exercise 56

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String reverse(String text) {
+        // write your code here
+        int i = text.length() - 1; // last symbol index
+        String reverseText = "";
+        while (i >= 0) {
+            reverseText += text.charAt(i);
+            i--;
+        }
+        return reverseText;
+    }
+
     public static void main(String[] args) {
-
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a word: ");
-        String word = reader.nextLine();
-        System.out.print("Length of the first part: ");
-        int length = Integer.parseInt(reader.nextLine());
-
-        System.out.println("Result: " + word.substring(0, length));
-
-
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
     }
 }
