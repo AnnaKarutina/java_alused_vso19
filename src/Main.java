9df24c557b316d6007acd883ab6c28960bef6896
Anna Karutina <anna.karutina@khk.ee>
2020-10-15 09:39:24 +0300

    Exercise 62

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int countItems(ArrayList<String> list) {
-        return list.size();
+    public static void removeLast(ArrayList<String> list) {
+        list.remove(list.size() - 1);
     }
 
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("Hallo");
-        list.add("Ciao");
-        list.add("Hello");
-        System.out.println("There are this many items in the list:");
-        System.out.println(countItems(list));
+        ArrayList<String> brothers = new ArrayList<String>();
+        brothers.add("Dick");
+        brothers.add("Henry");
+        brothers.add("Michael");
+        brothers.add("Bob");
+
+        System.out.println("brothers:");
+        System.out.println(brothers);
+
+        // sorting brothers
+        Collections.sort(brothers);
+        // brothers.sort();
+
+        // removing the last item
+        removeLast(brothers);
+
+        System.out.println(brothers);
     }
 }
